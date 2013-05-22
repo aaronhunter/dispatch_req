75b5e92b4649187454ec5adb43b55fc9e3af1dfd
Aaron Hunter <spuctor@gmail.com>
2013-05-21 22:31:53 -0500

    Create requests#show page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if user_signed_in?
+      @user = current_user
+      @requests = @user.requests.paginate(page: params[:page])
+    end
   end
 
   def help
